b4630cc2e5d85d4bb5d23769f95e80b2775ab84b
yangshangjun <saitohran@gmail.com>
2022-11-16 14:19:09 +0800

    Update diy1.sh

--- diy1.sh
+++ diy1.sh
@@ -17,7 +17,7 @@ sed -i "s/hostname='OpenWrt'/hostname='OpenWrt-Rex'/g" ./package/base-files/file
 curl -fsSL  https://raw.githubusercontent.com/firkerword/KPR/main/cus_config.yaml > ./package/openwrt-mos/luci-app-mosdns/root/etc/mosdns/cus_config.yaml
 # wget https://raw.githubusercontent.com/firkerword/KPR/main/cus_config.yaml -O ./package/openwrt-mos/luci-app-mosdns/root/etc/mosdns/cus_config.yaml
 # Modify default IP
-# sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 # rm -rf ./feeds/luci/applications/luci-app-qbittorrent
 rm -rf ./feeds/luci/applications/luci-app-serverchan
 # rm -rf ./package/diy-ziyong/adguardhome
