08fe2ef55be241f77201c718700f05a992e0cdd6
yangshangjun <saitohran@gmail.com>
2023-07-25 09:29:46 +0800

    Update 5.15.sh

--- 5.15.sh
+++ 5.15.sh
@@ -38,6 +38,10 @@ git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luc
 git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 #git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
+
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
+
 git clone https://github.com/sbwml/luci-app-alist.git package/luci-app-alist
 # git clone https://github.com/project-lede/luci-app-godproxy.git package/luci-app-godproxy
 # git clone https://github.com/garypang13/luci-app-dnsfilter.git package/luci-app-dnsfilter
