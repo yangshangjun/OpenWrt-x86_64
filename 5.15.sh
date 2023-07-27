44ee7327a2ebc70a15a2a01aabd286a27bbad718
yangshangjun <saitohran@gmail.com>
2023-07-27 12:33:18 +0800

    Update 5.15.sh

--- 5.15.sh
+++ 5.15.sh
@@ -13,7 +13,6 @@ sed -i 's/KERNEL_PATCHVER:=6.10/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Make
 
 svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt package/aliyundrive-webdav
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
-git clone https://github.com/sbwml/openwrt-alist.git package/openwrt-alist
 git clone https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-bypass package/luci-app-bypass
 git clone https://github.com/fw876/helloworld.git package/ssr
